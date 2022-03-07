18a1e72c041f506b18c24b687965d9b83678d77d
Abdulkadir Can Kinsiz <kadircankinsiz@gmail.com>
2022-03-07 05:10:15 +0200

    Proje dosyası ekle.

---
+++ SMS/DeviceRegister.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMS
+{
+    public partial class DeviceRegister : Form
+    {
+        Variables vars = new Variables();
+        List<string> deviceIdWithImei = new List<string>();
+        List<string> devices = new List<string>();
+        Process getDeviceIdProcess = new Process();
+        Process imeiProcess = new Process();
+        string selectedDeviceIdWithImei = "";
+        string selectedDeviceOldName = "";
+        public DeviceRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDevices()
+        {
+            DeviceListbox.Items.Clear();
+            try
+            {
+                if (deviceIdWithImei.Count > 0) { deviceIdWithImei.Clear(); }
+                if (devices.Count > 0) { devices.Clear(); }
+                //Get device id with adb.exe and read the output of cmd
+                getDeviceIdProcess.StartInfo.UseShellExecute = false;
+                getDeviceIdProcess.StartInfo.RedirectStandardOutput = true;
+                getDeviceIdProcess.StartInfo.CreateNoWindow = true;
+                getDeviceIdProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                getDeviceIdProcess.StartInfo.Arguments = "devices";
+                getDeviceIdProcess.StartInfo.FileName = vars.adbPath;
+                getDeviceIdProcess.Start();
+                string output = getDeviceIdProcess.StandardOutput.ReadToEnd();
+                getDeviceIdProcess.WaitForExit();
+                //Get device id with adb.exe and read the output of cmd
+
+                string[] lines = output.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);//Get all devices by dividing with new line
+                devices = lines.ToList();
+
+                devices.RemoveAt(0);//delete the first row whic is:List of attached devices(unnecessary)
+                for (int i = 0; i < devices.Count; i++)
+                {
+                    if (devices[i] == "" || devices[i] == " " || devices[i] == String.Empty || devices[i] == Environment.NewLine) devices.RemoveAt(i);
+                    {
+                        devices[i] = devices[i].Replace("device", "");
+                        devices[i] = devices[i].Replace(" ", "");
+                        devices[i] = devices[i].Replace("\t", "");
+                    }//remove the device word(unnecessary)
+                }
+                //Get device's imei number
+                foreach (string device in devices)
+                {
+                    if (!String.IsNullOrEmpty(device))
+                    {
+                        imeiProcess.StartInfo.UseShellExecute = false;
+                        imeiProcess.StartInfo.RedirectStandardOutput = true;
+                        imeiProcess.StartInfo.CreateNoWindow = true;
+                        imeiProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        imeiProcess.StartInfo.Arguments = device;
+                        imeiProcess.StartInfo.FileName = vars.imeiBatPath;
+                        imeiProcess.Start();
+                        imeiProcess.WaitForExit();
+                        string deviceImei = System.IO.File.ReadAllText(vars.imeisTxtPath);
+                        File.Delete(vars.imeisTxtPath);
+                        deviceIdWithImei.Add(device + ":" + deviceImei);
+                    }
+                }
+                //Get device's imei number
+                DeviceListbox.Items.Clear();
+                //listing devices
+                foreach (string device in deviceIdWithImei)
+                {
+                    DeviceListbox.Items.Add(device);
+                }
+                //listing devices
+
+            }
+            catch { }
+        }
+
+        private void DeviceRegister_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadDevices();
+            }
+            catch { }
+        }
+
+        private void DeviceRegister_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Process[] runingProcess = Process.GetProcessesByName("SMS");
+                for (int i = 0; i < runingProcess.Length; i++)
+                {
+                    runingProcess[i].Kill();
+                }
+            }
+            catch { }
+        }
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            LoadDevices();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string newRecord = DeviceNameText.Text + ":" + selectedDeviceIdWithImei.Split(':')[0] + ":" + selectedDeviceIdWithImei.Split(':')[1];
+            string[] lines = File.ReadAllLines(vars.savedDevicesPath);
+            if (selectedDeviceOldName != "")
+            {
+                string oldRecord = selectedDeviceOldName + ":" + selectedDeviceIdWithImei.Split(':')[0] + ":" + selectedDeviceIdWithImei.Split(':')[1];
+                
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    lines[i] = lines[i].Replace(" ","");
+                    lines[i] = lines[i].Replace("\t","");
+                    if (lines[i] == oldRecord)
+                    {
+                        lines[i] = newRecord;
+                    }
+                }
+            }
+            List<string> newLines = lines.ToList();
+            newLines.Add(newRecord);
+            File.WriteAllLines(vars.savedDevicesPath, newLines);
+            selectedDeviceOldName = "";
+        }
+
+        private void DeviceListbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                selectedDeviceIdWithImei = DeviceListbox.SelectedItem.ToString();
+                string deviceId = selectedDeviceIdWithImei.Split(':')[0];
+                string[] lines = File.ReadAllLines(vars.savedDevicesPath);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (!String.IsNullOrEmpty(lines[i]) && lines[i].Split(':')[1] == deviceId)
+                    {
+                        DeviceNameText.Text = lines[i].Split(':')[0];
+                        selectedDeviceOldName = lines[i].Split(':')[0]; break;
+                    }
+                }
+            }
+            catch { }
+
+        } 
+    }
+}
---
+++ SMS/MainPage.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace SMS
+{
+    partial class MainPage
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.deviceRegistiraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(459, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 329);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(459, 98);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(254, 99);
+            this.textBox2.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(459, 292);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(254, 134);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(86, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Devices";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add To List";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(361, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(459, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(254, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Send Now";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(539, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Numbers";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(361, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Message:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(459, 232);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Send All";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(587, 232);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Send Selected";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 18);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Selected Device Count:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(180, 396);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 18);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deviceRegistiraToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(816, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // deviceRegistiraToolStripMenuItem
+            // 
+            this.deviceRegistiraToolStripMenuItem.Name = "deviceRegistiraToolStripMenuItem";
+            this.deviceRegistiraToolStripMenuItem.Size = new System.Drawing.Size(120, 20);
+            this.deviceRegistiraToolStripMenuItem.Text = "Device Registration";
+            this.deviceRegistiraToolStripMenuItem.Click += new System.EventHandler(this.deviceRegistiraToolStripMenuItem_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(12, 423);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 18);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Devices Status:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(129, 423);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 18);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Checking...";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.ImageLocation = "C:\\Users\\Win\\source\\repos\\SMS\\SMS\\refresh.png";
+            this.pictureBox1.Location = new System.Drawing.Point(220, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 26);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(816, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainPage";
+            this.Text = "SMS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainPage_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem deviceRegistiraToolStripMenuItem;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
